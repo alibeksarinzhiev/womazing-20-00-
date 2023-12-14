200577872bdab6ba5a586b88e6d34479b1226a8a
alibek <alibek1024@mail.ru>
2023-12-14 20:50:19 +0600

    alibek 14

--- src/index.js
+++ src/index.js
@@ -2,16 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import {BrowserRouter} from "react-router-dom";
 import App from "./app";
-import {Context} from "./context";
+
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
         <BrowserRouter>
-            <Context>
                 <App/>
-            </Context>
         </BrowserRouter>
     </React.StrictMode>
 );
